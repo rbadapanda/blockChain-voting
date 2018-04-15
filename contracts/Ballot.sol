af499a7f58113c31cb194abca098c06a5ff3e2a8
rajenb <rbadapanda@gmail.com>
2018-04-15 15:26:20 -0700

    Simple - No delegation please

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -1,13 +1,12 @@
 pragma solidity ^0.4.16;
-///@title Voting with delegation. 
+///@title Simple Voting - no delegation. 
 contract Ballot {
     // This declares a new complex type which will 
     // be used for variables later.
     // It will represent a single voter.
     struct Voter {
-        uint weight; // weight is accumulated by delegation 
+        uint canVote; // canVote after being give(n)RightToVote
         bool voted; // if true, that person already voted 
-        address delegate; // person delegated to
         uint vote; // index of the voted proposal
     }
     // This is a type for a single proposal.
@@ -27,7 +26,7 @@ contract Ballot {
     ///Create a new ballot to choose one of `proposalNames`.
     function Ballot(bytes32[] proposalNames) public { 
         chairperson = msg.sender; 
-        voters[chairperson].weight = 1;
+        voters[chairperson].canVote = 1;
 
         // For each of the provided proposal names,
         // create a new proposal object and add it
@@ -55,51 +54,11 @@ contract Ballot {
         require(
             (msg.sender == chairperson) &&
             !voters[voter].voted &&
-            (voters[voter].weight == 0)
+            (voters[voter].canVote == 0)
         );
-        voters[voter].weight = 1;
+        voters[voter].canVote = 1;
     }
 
-    /// Delegate your vote to the voter `to`. 
-    function delegate(address to) public {
-        // assigns reference
-        Voter storage sender = voters[msg.sender]; 
-        require(!sender.voted);
-        
-        // Self-delegation is not allowed.
-        require(to != msg.sender);
-
-        // Forward the delegation as long as
-        // `to` also delegated.
-        // In general, such loops are very dangerous,
-        // because if they run too long, they might
-        // need more gas than is available in a block.
-        // In this case, the delegation will not be executed, 
-        // but in other situations, such loops might
-        // cause a contract to get "stuck" completely.
-        while (voters[to].delegate != address(0)) {
-            to = voters[to].delegate;
-            
-            // We found a loop in the delegation, not allowed.
-            require(to != msg.sender);
-        }
-        // Since `sender` is a reference, this 
-        // modifies `voters[msg.sender].voted` 
-        sender.voted = true;
-        sender.delegate = to;
-        Voter storage delegate_ = voters[to]; 
-        if (delegate_.voted) {
-            // If the delegate already voted,
-            // directly add to the number of votes 
-            proposals[delegate_.vote].voteCount += sender.weight;
-        } else {
-            // If the delegate did not vote yet, 
-            // add to her weight. 
-            delegate_.weight += sender.weight;
-        }
-    }
-
-    // Give your vote (including votes delegated to you)
     // to proposal `proposals[proposal].name`.
     function vote(uint proposal) public {
         Voter storage sender = voters[msg.sender]; 
@@ -110,7 +69,7 @@ contract Ballot {
         // If `proposal` is out of the range of the array, 
         // this will throw automatically and revert all
         // changes.
-        proposals[proposal].voteCount += sender.weight;
+        proposals[proposal].voteCount += sender.canVote;
     }
 
     // @dev Computes the winning proposal taking all
